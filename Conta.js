acaccbb082474eeb01683c602faa3b1cabd5cec1
Fabrizio <fabriziogomespereira@hotmail.com>
2021-11-25 11:58:53 -0300

    Concluindo a parte de Herança

--- Conta.js
+++ Conta.js
@@ -1,45 +1,46 @@
 export class Conta {
-    constructor(saldoInicial, cliente, agencia){
+    constructor(saldoInicial, cliente, agencia) {
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this._agencia = agencia;
-
+       
     }
 
-    set cliente(novoValor){
-        if(novoValor instanceof Cliente){
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
             this._cliente = novoValor;
         }
     }
 
-    get cliente(){
+    get cliente() {
         return this._cliente;
     }
 
-    get saldo (){
+    get saldo() {
         return this._saldo;
     }
 
-    sacar(valor){
+    sacar(valor) {
         let taxa = 1
-        const valorSacado = taxa * valor;
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
+        return this._sacar(valor, taxa);
     }
 
-    depositar(valor){
-        if(valor <=0){
-            return;
+    _sacar(valor, taxa){
+        const valorSacado = taxa * valor;
+        if (this._saldo >= valorSacado) {
+            this._saldo -= valorSacado;
+            return valorSacado;
         }
+
+        return 0;
+    }
+
+    depositar(valor) {
         this._saldo += valor;
     }
 
-    transferir(valor, conta){
+    tranferir(valor, conta) {
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
-
     }
-
 }
